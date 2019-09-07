ae546767a54df755eafdb46bfdc53e77000b7941
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-09-07 21:21:00 +0300

    init: create project ls7

---
+++ Ls7TimerControl/src/app/timer-control/timer-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-control',
+  templateUrl: './timer-control.component.html',
+  styleUrls: ['./timer-control.component.css']
+})
+export class TimerControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
