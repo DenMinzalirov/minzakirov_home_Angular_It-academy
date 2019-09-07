7ac564824397cde7b812cdb5c6f598c2cd2ed85e
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-09-07 11:29:46 +0300

    feat: task2-create button disabled

--- Task-1-Work_with_components/Alert/src/app/app.module.ts
+++ Task-1-Work_with_components/Alert/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { DataBindingWorkComponent } from './data-binding-work/data-binding-work.component';
 import {FormsModule} from "@angular/forms";
+import { ShowDetailsComponent } from './show-details/show-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
-    DataBindingWorkComponent
+    DataBindingWorkComponent,
+    ShowDetailsComponent
   ],
   imports: [
     BrowserModule,
--- Task-1-Work_with_components/Alert/src/app/data-binding-work/data-binding-work.component.ts
+++ Task-1-Work_with_components/Alert/src/app/data-binding-work/data-binding-work.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding-work',
   templateUrl: './data-binding-work.component.html',
-  styleUrls: ['./data-binding-work.component.css']
 })
 export class DataBindingWorkComponent implements OnInit {
   username = '';
