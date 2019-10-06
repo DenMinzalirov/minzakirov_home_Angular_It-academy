d9b41ff01fc89d2417f2227d22bfb29614a63ec0
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-10-06 23:57:03 +0300

    feat: inplment all valid

--- reactive-forms/src/app/reactive-form/form/form.component.ts
+++ reactive-forms/src/app/reactive-form/form/form.component.ts
@@ -1,27 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {of} from 'rxjs';
+import {delay, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent{
 
   constructor(private fb: FormBuilder) { }
 
   form = this.fb.group({
-    projectName: ['', Validators.required],
+    projectName: ['',
+      [
+        Validators.required,
+        // this.checkProjectName()
+      ],
+      [this.asyncCheck.bind(this)]
+    ],
     email: ['', [Validators.required, Validators.email]],
-    status: ['']
-  })
+    status: ['', Validators.required]
+  });
 
-  ngOnInit() {
+  options: string[] = ['Stable', 'Critical', 'Finished'];
+  test = 'Test';
+
+  private asyncCheck(control: AbstractControl) {
+    return of(this.test !== control.value).pipe(
+      delay(1000),
+      map(response => response ? null : {asyncCheck: true})
+    );
   }
+
+  // checkProjectName(): ValidatorFn {
+  //   return (control: AbstractControl): { [key: string]: boolean } | null => {
+  //     const valid = this.test !== control.value;
+  //     return valid ? null : { projectNameNoTest: true };
+  //   };
+  // }
 
   submitForm() {
     console.log('submit', this.form.value);
-    console.log('valid', this.form.valid);
   }
-
 }
